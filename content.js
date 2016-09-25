9d19501e4c565e36d5cccee0f1954aba973b5ade
Ikumi Shimizu <193s@users.noreply.github.com>
2016-09-25 18:59:21 +0900

    月を選択できるように

--- content.js
+++ content.js
@@ -25,10 +25,28 @@ if (location.pathname.endsWith('/unit')) {
           + ' (' + count_unfinished + ')');
 }
 else {
+  $('.list').children().each(function() {
+    var subject = $(this).children().eq(0).text();
+    subject = subject.replace('Ⅰ', '1').replace('Ｂ', 'B').replace('Ａ', 'A');
+    if (!data[subject]) {
+      console.error('[error] unknown subject:', JSON.stringify(subject));
+      return;
+    };
+    var subject_ok = -1;
+    $(this).find('.comp a, .normal a').each(function(cid) {
+      var x = $(this);
+      var title = x.text().replace(/^\s+|\s+$/g, '');
+      var i = 0;
+      for (i=0; i<months.length; i++) {
+        if (data[subject][months[i]] > cid) break;
+      }
+      x.text('《' + months[i] + '月》 ' + title);
+    });
+  });
+
   var updateList = function(month) {
     var ret = { num_ok: 0, num_ng: 0 };
-    var list = $('.list');
-    list.children().each(function() {
+    $('.list').last().children().each(function() {
       var subject = $(this).children().eq(0).text();
       subject = subject.replace('Ⅰ', '1').replace('Ｂ', 'B').replace('Ａ', 'A');
       if (!data[subject]) {
@@ -44,7 +62,6 @@ else {
           if (data[subject][months[i]] > cid) break;
         }
         var limit = months[i];
-        x.text('《' + limit + '月》 ' + title);
         // 今月締め切り
         if (limit <= month) {
           var base = x.parent().parent();
@@ -64,13 +81,20 @@ else {
             }
           }
         }
+        else {
+          var base = x.parent().parent();
+          base.css('background-color', '');
+        }
       });
       switch (subject_ok) {
-        case 1:
-          $(this).css('background-color', '#d9edf7');
+        case -1:
+          $(this).css('background-color', '');
           break;
         case 0:
           $(this).css('background-color', '#f2dede');
+          break;
+        case 1:
+          $(this).css('background-color', '#d9edf7');
           break;
       }
     });
@@ -82,8 +106,7 @@ else {
     select_tag.append( $('<option>').text( x + '月').val(x) );
   });
   select_tag.change(function() {
-    console.log('changed');
-    //updateList(5);
+    updateList($('select option:selected').val());
   });
   $('.list').before(select_tag);
 
